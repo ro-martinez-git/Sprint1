4f5393c626522917b96cd4c2c0903469a2495158
ro-martinez-git <roomartinez988@gmail.com>
2024-05-10 16:52:02 -0300

    creación de estructura y dependencia

---
+++ src/main/java/com/example/DesafioSprint1/DesafioSprint1Application.java
@@ -0,0 +1,13 @@
+package com.example.DesafioSprint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSprint1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSprint1Application.class, args);
+	}
+
+}
