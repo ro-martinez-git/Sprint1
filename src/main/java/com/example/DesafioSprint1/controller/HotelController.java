792d835079e9c08f1babbb8f24034060d80e5e5c
flazzaroni <franco.lazzaroni@sancorsalud.com.ar>
2024-05-16 14:07:26 -0300

    US 0003: Realizar una reserva de un hotel

--- src/main/java/com/example/DesafioSprint1/controller/HotelController.java
+++ src/main/java/com/example/DesafioSprint1/controller/HotelController.java
@@ -2,14 +2,14 @@ package com.example.DesafioSprint1.controller;
 
 import java.time.format.DateTimeFormatter;
 import com.example.DesafioSprint1.dto.HotelDTO;
+import com.example.DesafioSprint1.dto.BookingDTO;
+import com.example.DesafioSprint1.dto.Request.BookingRequestDTO;
+import com.example.DesafioSprint1.service.IBookingService;
 import com.example.DesafioSprint1.service.IHotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,6 +21,8 @@ public class HotelController {
     //Inyectar la dependencia
     @Autowired
     private IHotelService hotelService;
+    @Autowired
+    private IBookingService bookingService;
 
     @GetMapping("/hotels")
     public ResponseEntity<List<HotelDTO>> listHotels() {
@@ -33,6 +35,12 @@ public class HotelController {
         LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
         return new ResponseEntity<>(hotelService.availableHotels(localDateFrom, localDateTo, destination), HttpStatus.OK);
     }
+
+    @PostMapping("/booking")
+    public ResponseEntity<?> agregarAlumno(@RequestBody BookingRequestDTO bookingRequestDTO){
+        return new ResponseEntity<>(bookingService.makeBooking(bookingRequestDTO), HttpStatus.CREATED);
+    }
+
 
 }
 
