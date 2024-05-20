a66b6fff03b85c2e4083fd53a46580de3672c5e7
ro-martinez-git <roomartinez988@gmail.com>
2024-05-20 15:03:13 -0300

    excepción de fligth

--- src/main/java/com/example/DesafioSprint1/config/ExceptionHandlerController.java
+++ src/main/java/com/example/DesafioSprint1/config/ExceptionHandlerController.java
@@ -3,6 +3,7 @@ package com.example.DesafioSprint1.config;
 import com.example.DesafioSprint1.dto.ErrorDTO;
 import com.example.DesafioSprint1.exceptions.BookingRegistrationException;
 import com.example.DesafioSprint1.exceptions.EmptyFlightReservationException;
+import com.example.DesafioSprint1.exceptions.FlightNotFoundException;
 import com.example.DesafioSprint1.exceptions.ReservationInexistentException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,4 +30,10 @@ public class ExceptionHandlerController {
         ErrorDTO error = new ErrorDTO("Los datos de reserva no pueden estar vacios", 400);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(FlightNotFoundException.class)
+    public ResponseEntity<?> FlightNotFoundException(Exception e){
+        ErrorDTO error = new ErrorDTO("No hay vuelos para los datos especificados", 400);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
